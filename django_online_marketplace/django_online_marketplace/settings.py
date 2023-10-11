32f310ec675188822862c9caedc67cf380442b95
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-11 20:49:40 +0200

    Basic project configuration part 3
    loading home.html template, inside main app fodler creation of static folder and moving all the template-related static files into it, configuring
    the static files inside main apps settings.py, loading static inside the home.html template, running collect static

--- django_online_marketplace/django_online_marketplace/settings.py
+++ django_online_marketplace/django_online_marketplace/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [
+    'django_online_marketplace/static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
