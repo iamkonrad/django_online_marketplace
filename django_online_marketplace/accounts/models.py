ce969875543869593d12fbd3a19e37884bae4e6d
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-12 19:52:28 +0200

    UserProfile model
    Inside accounts app creation of a user profile model: creating a one-to -one relationship with the user model and fields such as profile_picture, cover_photo, address etc. , migrating the newly created model, registering the model inside the admin.py

--- django_online_marketplace/accounts/models.py
+++ django_online_marketplace/accounts/models.py
@@ -68,3 +68,21 @@ class User(AbstractBaseUser):
 
     def has_module_perms(selfself,app_label):
         return True
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True,null=True)
+    profile_picture = models.ImageField(upload_to='media/users/profile_pictures',blank=True,null=True)
+    cover_photo = models.ImageField(upload_to='media/users/cover_photos',blank=True,null=True)
+    address_line_1 = models.CharField(max_length=150,blank=True,null=True)
+    address_line_2 = models.CharField(max_length=150,blank=True,null=True)
+    country = models.CharField(max_length=150,blank=True,null=True)
+    province = models.CharField(max_length=150,blank=True,null=True)
+    city = models.CharField(max_length=150, blank=True, null=True)
+    postcode = models.CharField(max_length=50,blank=True,null=True)
+    longitude = models.CharField(max_length=30,blank=True,null=True)
+    latitude = models.CharField(max_length=30,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
