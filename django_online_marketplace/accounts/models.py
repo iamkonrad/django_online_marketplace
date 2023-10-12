457fcfa55f605d1d78176ef94c9d50b669da090e
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-12 21:00:52 +0200

    Automatic User Profile creation using django signals
    creation of signals.py file inside the accounts app; creation of a post_save_create_profile_receiver function using receiver decorator in order to
    connect receiver and sender(user), once a user instance is created user profile will get automatically created as well; otherwise (else) the try statement will first try to fetch it, to check if there is an existing
    userprofile instance (in case of wanting to update it), otherwise, if there is no existing userprofile it will be automatically created; creation of a pre_save_profile_receiver function that will inform us that a user
    profile is being correctly saved in the db; inside apps.py creation of a custom ready function;

--- django_online_marketplace/accounts/models.py
+++ django_online_marketplace/accounts/models.py
@@ -3,6 +3,7 @@ from .choices import ROLE_CHOICE
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+
 class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, username,email, password=None):
         if not email:
---
+++ django_online_marketplace/accounts/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save, pre_save
+from accounts.models import User, UserProfile
+from django.dispatch import receiver
+
+
+@receiver (post_save,sender=User)                                                                                       #connecting receiver and sender using decorator
+def post_save_create_profile_receiver(sender, instance, created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            UserProfile.objects.create(user=instance)
+        print ('The user profile has been updated)')
+
+@receiver(pre_save, sender=User)
+def pre_save_profile_receiver(sender, instance, **kwargs):
+    print(instance.username, 'This user is being saved.')
