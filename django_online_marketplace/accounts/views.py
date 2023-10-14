5c09d79c1ad57e03ea53e7f55d714ce5b0f154c8
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-14 03:31:39 +0200

    User registration form: secure password storing
    Inside accounts app registeruser function calling the create user method of UserManager class of accounts apps models in order to create a new user instance and
    store the newly created password in a hashed form

--- django_online_marketplace/accounts/views.py
+++ django_online_marketplace/accounts/views.py
@@ -8,7 +8,13 @@ def registeruser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data ['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email = email,
+                                            password=password)
             user.role = 2
             user.save()
             return redirect('registeruser')
