55d122a47cf2e5767d12ad8c84cc1438ca63eded
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-16 03:33:06 +0200

    Register vendor functionality
    Inside registervendor.html adding a url path reference to registervendor; Inside the vendor app creation of forms.py file, therein creating the fields responsible for capturing the
    vendor's name and license; Inside the account apps views  registervendor function adding the variables corresponding to the vendorregistration form fields, adding an if check for the method, if the request is
    made via POST it will be used to send form's data, if it's sent via GET request the form will simply get rendered, nesting an additional if check inside the function to check if the forms used in the
    registervendor are valid, in case they are the instance of vendor account will be created using the clean method, if the forms are invalid an error message will be displayed; ; inside registervendor.html adding the reference to the vendor
    form fields such as vendor_name and vendor license, setting the correct form path reference to lead to registervendor and  enc type of the form to correctly handle uploading of the file containing the vendor's
    license

--- django_online_marketplace/accounts/views.py
+++ django_online_marketplace/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+
+from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 
@@ -19,6 +21,9 @@ def registeruser(request):
             user.save()
             messages.success(request, 'Your account has been successfully registered.')
             return redirect('registeruser')
+        else:
+            print('invalid form')
+            print(form.errors)
     else:
         form = UserForm()
     context = {
@@ -28,4 +33,37 @@ def registeruser(request):
 
 
 def registervendor(request):
-    return render (request, 'accounts/registervendor.html')
+    if request.method =='POST':
+        form = UserForm(request.POST)
+        vendor_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and vendor_form.is_valid:
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email,
+                                            password=password)
+            user.role = 1
+            user.save()
+            vendor = vendor_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your vendor account has been successfully registered.')
+            return redirect('registervendor')
+        else:
+            print('invalid form')
+            print(form.errors)
+
+    else:
+        form = UserForm()
+        vendor_form = VendorForm()
+
+    context = {
+        'form':form,
+        'vendor_form':vendor_form,
+    }
+
+    return render (request, 'accounts/registervendor.html', context)
