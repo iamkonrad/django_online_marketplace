e19b0dccd53557f4722bdeab65ebb8e5ec9fc662
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-24 20:08:42 +0200

    Vendor profile part:4 Update profile data
    Inside vendor app's views vendorprofile function adding an if check in order to validate profile and vendor forms, in case the forms are valid they will be saved and the success
    messages displayed, in case forms are invalid the user will be redirected back to dashboard and error message displayed

--- django_online_marketplace/vendor/views.py
+++ django_online_marketplace/vendor/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
 from vendor.forms import VendorForm
 from vendor.models import Vendor
+from django.contrib import messages
 
 
 def vendorprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
 
-    profile_form = UserProfileForm(instance = profile)
-    vendor_form = VendorForm(instance = vendor)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST,request.FILES,instance=profile)
+        vendor_form = VendorForm(request.POST,request.FILES,instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request,'Your data has been saved. ')
+            return redirect('vendorprofile')
+        else:
+            messages.error(request, 'There was an error. Please try again ')
+            return redirect('vendordashboard')
+
+    else:
+        profile_form = UserProfileForm(instance = profile)
+        vendor_form = VendorForm(instance = vendor)
 
     context = {
         'profile_form': profile_form,
