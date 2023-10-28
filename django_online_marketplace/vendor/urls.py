d89db4150827ac55325741a5d1ea781f94472340
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-10-28 16:50:30 +0200

    Delete Category (vendor)
    Inside vendor app's view creation of delete_category function responsible for deleting based on the pk; Inside the menu_builder.html linking bin icon with function's path, loading js CONFIRM messages into the template

--- django_online_marketplace/vendor/urls.py
+++ django_online_marketplace/vendor/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('menu-builder/', views.menu_builder, name='menu-builder'),
     path('menu-builder/category/<int:pk>/', views.products_by_category,name='products_by_category'),
 
+    #CATEGORY CRUD
     path('menu-builder/category/add',views.add_category, name='add_category'),
-    path('menu-builder/category/EDIT/<int:pk>/', views.edit_category, name='edit_category'),
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete_category'),
+
+    #PRODUCT CRUD
 
 ]
