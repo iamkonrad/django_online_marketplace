be33d0202fa2876eab11e685c087672b17db235a
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-03 21:03:55 +0100

    Cart: add to cart:logic
    Inside marketplace views add_to_cart adding an if check for a json response that will first ascertain whether a user is logged in, in case he is a check if the request was sent using AJAX is
    involved, if that's the case the product with specific product_id will be retrieved; in case there is no existing cart a new cart instance will get created associated with a particular logged-in user, in case a particular
    product doesn't exist in the db the user will be notified about it as well

--- django_online_marketplace/marketplace/views.py
+++ django_online_marketplace/marketplace/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Prefetch
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse
 
+from marketplace.models import Cart
 from menu.models import Category, Product
 from vendor.models import Vendor
 
@@ -34,5 +35,24 @@ def vendor_detail(request, vendor_slug):
     return render(request, 'marketplace/vendor_detail.html', context)
 
 
-def add_to_cart(request, product_id= None):
-    return HttpResponse(product_id)
+def add_to_cart(request, product_id):
+    if request.user.is_authenticated:
+        if request.is_ajax():
+            try:
+                product = Product.objects.get(id=product_id)
+                try:
+                    checkCart = Cart.objects.get(user=request.user,product=product)
+                    checkCart.quantity += 1
+                    checkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Cart quantity has been increased.'})
+                except:
+                    checkCart = Cart.objects.create(user=request.user,product=product, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'The product has been added to the cart.'})
+
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This product does not exist.'})
+
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request.'})
+    else:
+        return JsonResponse({'status':'Failed', 'message':'Please login to continue.'})
--- django_online_marketplace/static/js/custom.js
+++ django_online_marketplace/static/js/custom.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
             url:url,
             data: data,
             success: function(response){
-                alert(response)
+                console.log(response)
             }
         })
     })
