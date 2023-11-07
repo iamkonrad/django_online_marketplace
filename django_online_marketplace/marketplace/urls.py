bb742e3c65912cf8fd35b9a4680e66b174710dad
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-07 09:50:41 +0100

    Cart items: part:1 delete cart items basic setup
    Inside the marketplace app creation of delete_cart function and its corresponding url, the function first checks if user is authenticated and request was submitted via
    ajax, inside a try except block cart item belonging to a particular user will get retrieved and afterwards removed displaying a success message, in case a request wasn't submitted via ajax or the items don't exist in a cart
    a failure message will be displayed

--- django_online_marketplace/marketplace/urls.py
+++ django_online_marketplace/marketplace/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     #CRUD
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('decrease_cart/<int:product_id>/', views.decrease_cart, name='decrease_cart'),
+    path('delete_cart/<int:cart_id>/', views.delete_cart, name='delete_cart'),
 
     #View
     path('cart/', views.cart, name='cart'),
--- django_online_marketplace/marketplace/views.py
+++ django_online_marketplace/marketplace/views.py
@@ -99,3 +99,18 @@ def cart(request):
         'cart_items':cart_items,
     }
     return render(request, 'marketplace/cart.html',context )
+
+def delete_cart(request, cart_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            try:
+                cart_item = Cart.objects.get(user=request.user,id=cart_id)
+                if cart_item:
+                    cart_item.delete()
+                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted',
+                                         'cart_counter':get_cart_counter(request)})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'Cart item does not exist'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request.', })
+
