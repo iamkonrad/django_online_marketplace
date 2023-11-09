c21e48f9376adc8df7eeee3156cfa12f9d07fb5d
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-09 13:43:06 +0100

    Cart: part 8; calculate subtotal,grand total in context processor
    Inside the context_processor creation of get_cart_amount function that will initialize subtotal, tax and grand total and retrieve products for a particular authenticated user, then looping over all the
    products in the cart by its pk and subtotal, finally returning subtotal, tax and grand total as a dictionary, registering the function in the settings/(context processor)

--- django_online_marketplace/marketplace/context_processors.py
+++ django_online_marketplace/marketplace/context_processors.py
@@ -15,3 +15,16 @@ def get_cart_counter(request):
         except:
             cart_count = 0
     return dict(cart_count=cart_count)
+
+def get_cart_amount(request):
+    subtotal = 0
+    tax = 0
+    grand_total = 0
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.user
+        for item in cart_items:
+            product = Product.objects.get(pk=item.product.id)
+            subtotal += (product.price * item.quantity)
+
+        grand_total = subtotal + tax
+    return dict(subtotal=subtotal, tax=tax, grand_total = grand_total)
