b11ee7fa378a17835d946d2488aa18d5a21b0da4
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-14 16:46:44 +0100

    Opening hours: open/closed vendor_detail.html
    Inside the marketplace views vendor_detail function adding a for loop for current_time that will iterate over the start and end hour utilizing the datetime method, adding an if check
    within the function checking if the current time is higher than the start (opening time) and not exceeding the end (closing) time, if that's the case the is_open label will get activated, otherwise is_open will be marked as
    False, passing the is_open into the context dictionary; Inside the vendor_detail.html

--- django_online_marketplace/marketplace/views.py
+++ django_online_marketplace/marketplace/views.py
@@ -8,7 +8,7 @@ from marketplace.models import Cart
 from menu.models import Category, Product
 from vendor.models import Vendor, OpeningHours
 
-from datetime import date
+from datetime import date, datetime
 
 
 def marketplace (request):
@@ -38,6 +38,17 @@ def vendor_detail(request, vendor_slug):
     today = today_date.isoweekday()
 
     current_opening_hours = OpeningHours.objects.filter(vendor=vendor,day=today)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    is_open = None
+    for i in current_opening_hours:
+        start = str(datetime.strptime(i.from_hour,"%I:%M %p").time())
+        end = str(datetime.strptime(i.to_hour, "%I:%M %p").time())
+        if current_time > start and current_time < end:
+            is_open = True
+        else:
+            is_open = False
 
 
     if request.user.is_authenticated:
@@ -51,6 +62,7 @@ def vendor_detail(request, vendor_slug):
         'cart_items': cart_items,
         'opening_hours':opening_hours,
         'current_opening_hours':current_opening_hours,
+        'is_open':is_open,
     }
 
     return render(request, 'marketplace/vendor_detail.html', context)
