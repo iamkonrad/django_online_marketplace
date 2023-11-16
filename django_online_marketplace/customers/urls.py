0c00c1b4888af08f4249a734aa5e04091139eebd
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-16 16:17:13 +0100

    Customers app, initial function, template
    Creation of customer_profile function, its corresponding path  and styling the customer_profile.html template

--- django_online_marketplace/customers/urls.py
+++ django_online_marketplace/customers/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from  accounts import views as AccountViews
+from accounts import views as AccountViews
+from. import views
 
 
 
 urlpatterns = [
     path('', AccountViews.customerdashboard,name='customer'),
+    path('profile/',views.customer_profile,name='customer_profile'),
 
 
 
--- django_online_marketplace/customers/views.py
+++ django_online_marketplace/customers/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def customer_profile(request):
+    return render(request, 'customers/customer_profile.html')
