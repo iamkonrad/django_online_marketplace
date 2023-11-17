3b5c6510161aeba5a849169ff539df0240e8ccd9
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-17 22:03:19 +0100

    Customers: authenticated/valid if checks
    Inside the customer app's customer_profile function adding if checks checking whether profile and user_form have been sent using POST method and whether they are valid, in case
    they meet the reuqirements a success message will be displayed, in case the forms don't meet the requirement of being valid an error will be printed, in case they were not sent via a POST request the autopopulated
    forms will be regenerated

--- django_online_marketplace/customers/urls.py
+++ django_online_marketplace/customers/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from accounts import views as AccountViews
-from. import views
-
+from . import views
 
 
 urlpatterns = [
-    path('', AccountViews.customerdashboard,name='customer'),
-    path('profile/',views.customer_profile,name='customer_profile'),
+    path('', AccountViews. customerdashboard, name='customer'),
+    path('profile/', views.customer_profile, name='customer_profile'),
 
 
 
