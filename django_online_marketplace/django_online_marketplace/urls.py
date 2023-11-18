1845a3f4c33bad097d1a92329d0f2b0431c7ff5c
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-11-18 21:37:28 +0100

    Orders: cart, checkout
    Inside the marketplace views creation of a checkout function, its corresponding url path and template; Inside cart.html adding additional "proceed to checkout element", connecting proceed to
    checkout element to checkout url path

--- django_online_marketplace/django_online_marketplace/urls.py
+++ django_online_marketplace/django_online_marketplace/urls.py
@@ -35,4 +35,7 @@ urlpatterns = [
     # Search
     path('search/',MarketplaceViews.search,name='search'),
 
+    # Checkout
+    path('checkout/', MarketplaceViews.checkout, name='checkout'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_online_marketplace/marketplace/views.py
+++ django_online_marketplace/marketplace/views.py
@@ -141,3 +141,6 @@ def search(request):
     }
 
     return render(request, 'marketplace/listings.html',context)
+
+def checkout(request):
+    return render(request, 'marketplace/checkout.html')
